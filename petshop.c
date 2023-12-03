1c1b11ed1b237b6b95552bd1e98e0d908c982c22
Lucas Souza <126576008+LucasSD540@users.noreply.github.com>
2023-12-03 18:01:28 -0300

    Update petshop.c

--- petshop.c
+++ petshop.c
@@ -3,10 +3,14 @@
 #include<string.h>
 #define SIZE 10
 
-char senha[50], nomeCliente[SIZE][50], nomePet[SIZE][50], tel[SIZE][50];
-int menu, clientes;
+char senha[50], nomeCliente[SIZE][50], nomePet[SIZE][50], tel[SIZE][50], nomePetAgendamento[SIZE][50], clienteAgendado[SIZE][50], clienteConsulta[SIZE][50], drConsulta[SIZE][50];
+int menu, clientes, agendamento, agendamentos, consulta, consultas, data, cliente, dr;
 
 void cadastrarCliente();
+
+void agendar();
+
+void consultar();
 
 void imprimir();
 
@@ -40,6 +44,115 @@ void seu_primeiro_nome() {
     while(menu == 1);
 }
 
+void agendar() {
+	static int linha;
+	
+	printf("Super_Petshop_Lucas_ADS\n\n");
+	
+	do {
+        printf("Nome do pet: ");
+        fflush(stdin);
+        fgets(nomePetAgendamento[linha], 50, stdin);
+
+        printf("Serviços disponíveis: \n");
+        printf("1 - Banho\n");
+        printf("2 - Tosa\n");
+        printf("3 - Banho e tosa\n");
+        fflush(stdin);
+        scanf("%i", &agendamento);
+        
+        switch(agendamento) {
+        	case 1:
+        		sprintf(clienteAgendado[linha], "%s - Banho", nomePetAgendamento[linha]);
+        	break;
+        	case 2:
+        		sprintf(clienteAgendado[linha], "%s - Tosa", nomePetAgendamento[linha]);
+        	break;
+        	case 3:
+        		sprintf(clienteAgendado[linha], "%s - Banho e tosa", nomePetAgendamento[linha]);
+        	break;
+        	default:
+        		printf("Opção incorreta");
+        	break;
+		}
+
+		linha++;
+		
+		agendamentos++;
+		
+        system("cls");
+        menuShow();
+    }
+    while(menu == 2);
+}
+
+void consultar() {
+	static int linha;
+	int i;
+	
+	printf("Super_Petshop_Lucas_ADS\n\n");
+	
+	do {
+        printf("Dias disponíveis: \n");
+        printf("1 - 06/12\n");
+        printf("2 - 07/12\n");
+        fflush(stdin);
+        scanf("%i", &data);
+
+	printf("\nSelecione qual cliente você é: \n");
+
+       for(i = 0; i < clientes; i++) {
+    		printf("\n%i - %s", nomeCliente[i], i + 1);
+	   }
+        fflush(stdin);
+        scanf("%i", &cliente);
+        
+        switch(cliente) {
+        	case 1:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[i]);
+        	break;
+        	case 2:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[i]);
+        	break;
+        	case 3:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[i]);
+        	break;
+        	default:
+        		printf("Opção incorreta");
+        	break;
+		}
+        
+    	printf("\n1 - Dr. Lucas");
+    	printf("\n2 - Dr. Fabiano");
+	   
+        fflush(stdin);
+        scanf("%i", &dr);
+        
+        switch(dr) {
+        	case 1:
+        		sprintf(drConsulta[linha], "%s - Banho", clienteConsulta[linha]);
+        	break;
+        	case 2:
+        		sprintf(drConsulta[linha], "%s - Tosa", clienteConsulta[linha]);
+        	break;
+        	case 3:
+        		sprintf(drConsulta[linha], "%s - Banho e tosa", clienteConsulta[linha]);
+        	break;
+        	default:
+        		printf("Opção incorreta");
+        	break;
+		}
+
+		linha++;
+		
+		consultas++;
+		
+        system("cls");
+        menuShow();
+    }
+    while(menu == 3);
+}
+
 void imprimir() {
 	int i;
 	
@@ -47,9 +160,11 @@ void imprimir() {
 	
 	for(i = 0; i < clientes; i++) {
 		printf("\nNome do cliente: %s", nomeCliente[i]);
-		printf("\nNome do pet cliente: %s", nomePet[i]);
-		printf("\nTelefone do cliente: %s", tel[i]);
-		printf("\n");
+	}
+	system("pause");
+	
+	for(i = 0; i < agendamentos; i++) {
+		printf("\n%s", clienteAgendado[i]);
 	}
 	system("pause");
 }
@@ -67,22 +182,24 @@ void menuShow() {
 		
 		switch(menu) {
 			case 1 :
-					system("cls");
-					seu_primeiro_nome();
-				break;
+				system("cls");
+				seu_primeiro_nome();
+			break;
 			;
 			case 2 :
 				system("cls");
-				break;
+				agendar();
+			break;
 			;
 			case 3 :
 				system("cls");
-				break;
+				consultar();
+			break;
 			;
 			case 4 :
 				system("cls");
 				imprimir();
-				break;
+			break;
 			;
 		}
 }
