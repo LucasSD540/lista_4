bc097fede2bc191514ee0469ea80bd83c228190e
Lucas Souza <126576008+LucasSD540@users.noreply.github.com>
2023-12-03 23:03:47 -0300

    Update petshop.c

--- petshop.c
+++ petshop.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<locale.h>
 #include<string.h>
 #define SIZE 10
 
-char senha[50], nomeCliente[SIZE][50], nomePet[SIZE][50], tel[SIZE][50], nomePetAgendamento[SIZE][50], clienteAgendado[SIZE][50], clienteConsulta[SIZE][50], drConsulta[SIZE][50];
+char senha[50], nomeCliente[SIZE][50], nomePet[SIZE][50], tel[SIZE][50], nomePetAgendamento[SIZE][50], clienteAgendado[SIZE][50], clienteConsulta[SIZE][50], drConsulta[SIZE][50], finalConsulta[SIZE][50], dataConsulta[50];
 int menu, clientes, agendamento, agendamentos, consulta, consultas, data, cliente, dr;
 
 void cadastrarCliente();
@@ -13,6 +14,8 @@ void agendar();
 void consultar();
 
 void imprimir();
+
+void sair();
 
 void menuShow();
 
@@ -99,49 +102,86 @@ void consultar() {
         fflush(stdin);
         scanf("%i", &data);
 
-	printf("\nSelecione qual cliente você é: \n");
+	if(data == 1) {
+		strcpy(dataConsulta, "06/12");
+	} else if(data == 2) {
+		strcpy(dataConsulta, "07/12");
+	} else {
+		system("cls");
+		printf("Data inválida\n");
+		consultar();
+		system("cls");
+	}
+
+	printf("\nLista de clientes: \n");
 
        for(i = 0; i < clientes; i++) {
-    		printf("\n%i - %s", nomeCliente[i], i + 1);
+    		printf("\n%i - %s", i + 1, nomeCliente[i]);
 	   }
+	   
+	   printf("Escolha um dos clientes. \n");
         fflush(stdin);
         scanf("%i", &cliente);
         
         switch(cliente) {
         	case 1:
-        		sprintf(clienteConsulta[linha], "%s", nomeCliente[i]);
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[0]);
         	break;
         	case 2:
-        		sprintf(clienteConsulta[linha], "%s", nomeCliente[i]);
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[1]);
         	break;
         	case 3:
-        		sprintf(clienteConsulta[linha], "%s", nomeCliente[i]);
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[2]);
+        	break;
+        	case 4:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[3]);
+        	break;
+        	case 5:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[4]);
+        	break;
+        	case 6:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[5]);
+        	break;
+        	case 7:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[6]);
+        	break;
+        	case 8:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[7]);
+        	break;
+        	case 9:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[8]);
+        	break;
+        	case 10:
+        		sprintf(clienteConsulta[linha], "%s", nomeCliente[9]);
         	break;
         	default:
         		printf("Opção incorreta");
         	break;
 		}
         
-    	printf("\n1 - Dr. Lucas");
-    	printf("\n2 - Dr. Fabiano");
+        printf("\nLista de Drs.: \n");
+        
+    	printf("\n1 - Dr. Lucas \n");
+    	printf("\n2 - Dr. Fabiano \n");
+	   
+	   printf("\nEscolha um dos Drs.\n");
 	   
         fflush(stdin);
         scanf("%i", &dr);
         
         switch(dr) {
         	case 1:
-        		sprintf(drConsulta[linha], "%s - Banho", clienteConsulta[linha]);
+        		sprintf(drConsulta[linha], "Dr. Lucas");
         	break;
         	case 2:
-        		sprintf(drConsulta[linha], "%s - Tosa", clienteConsulta[linha]);
-        	break;
-        	case 3:
-        		sprintf(drConsulta[linha], "%s - Banho e tosa", clienteConsulta[linha]);
+        		sprintf(drConsulta[linha], "Dr. Fabiano");
         	break;
         	default:
         		printf("Opção incorreta");
         	break;
 		}
+		
+		sprintf(finalConsulta[linha], "%s / %s / %s", drConsulta[linha], clienteConsulta[linha], dataConsulta);
 
 		linha++;
 		
@@ -158,15 +198,33 @@ void imprimir() {
 	
 	printf("Super_Petshop_Lucas_ADS\n\n");
 	
+	printf("\n\n----------clientes----------\n\n");
+	
 	for(i = 0; i < clientes; i++) {
-		printf("\nNome do cliente: %s", nomeCliente[i]);
+		printf("\n%i - Nome do cliente: %s\n", i + 1, nomeCliente[i]);
 	}
-	system("pause");
+	
+	printf("\n\n----------agendamentos----------\n\n");
 	
 	for(i = 0; i < agendamentos; i++) {
-		printf("\n%s", clienteAgendado[i]);
+		printf("\n%i %s\n", i + 1, clienteAgendado[i]);
 	}
+	
+	printf("\n\n----------consultas----------\n\n");
+	
+	for(i = 0; i < consultas; i++) {
+		printf("\n%i - %s\n", i + 1, finalConsulta[i]);
+	}
+	
 	system("pause");
+}
+
+void sair() {
+	system("cls");
+	
+	printf("Volte sempre ao Super_Petshop_Lucas_ADS");
+	
+	exit(0);
 }
 
 void menuShow() {
@@ -176,9 +234,18 @@ void menuShow() {
 		
 		printf("1 - Cadastrar Cliente\n");
 		printf("2 - Agendar BANHO ou TOSA\n");
-		printf("3 - CONSULTA\n");
-		printf("4 - IMPRIMIR RELATÓRIO GERAL\n");
+		printf("3 - Consulta\n");
+		printf("4 - Imprimir relatório geral\n");
+		printf("5 - Sair\n");
+		fflush(stdin);
 		scanf("%i", &menu);
+		
+		if(menu != 1 && menu != 2 && menu != 3 && menu != 4 && menu != 5) {
+			system("cls");
+			printf("Opção incorreta\n");
+			system("pause");
+			menuShow();
+		}
 		
 		switch(menu) {
 			case 1 :
@@ -199,6 +266,11 @@ void menuShow() {
 			case 4 :
 				system("cls");
 				imprimir();
+			break;
+			;
+			case 5 :
+				system("cls");
+				sair();
 			break;
 			;
 		}
